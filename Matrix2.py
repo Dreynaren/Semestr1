99bdcf8b8153ec3aac3512d6ebf868a1a03b3787
Dreynaren <92063692+Dreynaren@users.noreply.github.com>
2022-01-25 13:26:03 +1000

    Update Matrix2.py

--- Matrix2.py
+++ Matrix2.py
@@ -1,59 +1,52 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-m=[1, -1]
-n=int(input('размер матрицы '))
-a=np.zeros((n,n))
-h=int(input('количество повторов '))
-l=0 
-per=int(input('нешахматы(1) или шахматы(2)'))
-for i in range(n):
-    for j in range(n):
-        a[i, j]=random.choice(m)
-print(a)                                   
-c=a.copy()
 
-for l in range(h):
-    print("Петля №", l)
-    b=a.copy()
-    summ1=0
-    summ2=0
-    b[random.randrange(n), random.randrange(n)]*=-1 
-    for i in range(n):
-        for j in range(n):
-            summ1+=a[i, j]*a[i, j-1]+a[i-1, j]*a[i, j]
-            summ2+=b[i, j]*b[i, j-1]+b[i-1, j]*b[i, j]
-    print('Сумма новой ',summ2)
-    if per==1:
-        dsumm=-1*(summ2-summ1)
+T = float(input('Введите Т: '))
+x = int(input('input size: '))
+C_N = range(x)
+matrix = np.zeros((x, x))
+for i in range(x):
+    for j in range(x):
+        matrix[i][j] = random.choice([-1,1])
+print(matrix)
+
+
+mas = []
+mas = matrix.copy()
+for i in range(int(input('input iteration count: '))):
+    E = E1 = 0
+    for j in range(x):
+        for k in range(x):
+            E += matrix[j][k] * matrix[j][k - 1] + matrix[j - 1][k] * matrix[j][k]
+            #E*=-1
+    mas1 = []
+    mas1 = matrix.copy()
+    mas1[random.choice(C_N)][random.choice(C_N)] *= -1
+    for j in range(x):
+        for k in range(x):
+            E1 += mas1[j][k] * mas1[j][k - 1] + mas1[j - 1][k] * mas1[j][k]
+            #E1*=-1
+    deltaE = -1 * (E1 - E)
+
+    if deltaE <= 0:
+        matrix = mas1.copy()
+        mas = matrix.copy()
     else:
-        dsumm=summ2-summ1
-    print('Разницы сумм ',dsumm)                                   
-    if dsumm<=0:                                       
-        a=b.copy()
-    else:
-        t=float(1)
-        w=np.exp(-dsumm/t)
-        p=round(random.uniform(0, 1), 5)
-        if w>=p:
-            c=a.copy()
-        elif w<0:
-            a=c.copy()
-    i+=1
+        
+        P = random.random()
+        W = np.exp(-deltaE/T)
+        if P <= W:
+            mas = matrix.copy()
+        else:
+            matrix = mas.copy()
+    #print(matrix)
     plt.clf()
-    plt.imshow(a)
+    plt.imshow(matrix)
     plt.draw()
     plt.show()
     plt.gcf().canvas.flush_events()
-
-
-# In[ ]:
+plt.ioff()
 
 
 
